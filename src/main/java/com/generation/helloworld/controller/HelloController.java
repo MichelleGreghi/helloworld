f39eae9d412f9e36fb66029ce6ed09dd845bf323
Michelle Greghi <michelle.neves23@gmail.com>
2023-04-12 15:37:45 -0300

    Spring

--- src/main/java/com/generation/helloworld/controller/HelloController.java
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -17,4 +17,20 @@ public class HelloController {
 	public String meuNome() {
 		return "<b>Michelle</b>";
 	}
+	
+	@GetMapping("/bsm")
+	public String bsm() {
+		return "<b>Mentalidade de Crescimento - Persistência - Responsabilidade Pessoal</b><br><br>"
+				+ "<b>Orientação ao Futuro - Comunicação - Orientação ao Detalhe</b><br><br>"
+				+ "<b>Proatividade - Trabalho em Equipe - Dar e Receber - Gestão de Tempo</b><br><br>";	
+	}
+	
+	@GetMapping("/aprendizagem")
+	public String aprendizagem() {
+		return "<b>Mentalidade de Crescimento</b><br><br>"
+				+ "Meu objetivo de aprendizagem essa semana é na questão da Mentalidade de Crescimento, como estamos entrando em um novo projeto,"
+				+ " parece ser muito complexo e dificil, mas com a Mentalidade de Crescimento consigo focar no fato de que mesmo que seja novo, conseguirei absorver da melhor forma,"
+				+ " dando o meu melhor no quesito concetração e tirando dúvidas quando elas surgirem, para que no final do projeto tudo ocorra conforme os planos.";	
+	}
+	
 }
